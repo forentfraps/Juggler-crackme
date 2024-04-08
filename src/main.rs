6bd1f52d0cd60028c8bcb7c2e60e82731d16ad72
The One The Master <vacpox@gmail.com>
2024-04-08 17:20:50 +0100

    some more additions to custom winapi core

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use winapi::shared::minwindef::{BYTE, DWORD, HMODULE, ULONG, WORD};
 use winapi::shared::ntdef::{LPCSTR, LPCWSTR, PVOID, ULONGLONG, UNICODE_STRING};
 use winapi_cs::core::*;
 
-type func = fn(()) -> ();
+type func = fn() -> ();
 
 fn main() {
     let s = String::from("C:\\Windows\\System32\\ntdll.dll");
@@ -17,7 +17,7 @@ fn main() {
         let ntdll: HMODULE = GetModuleHandle(pws).unwrap();
         //hide!();
         let f_ptr: func = GetProcAddress(ntdll, ps2).unwrap();
-        f_ptr(());
+        f_ptr();
     }
     println!("Hello, world!");
 }
--- src/winapi_cs/core.rs
+++ src/winapi_cs/core.rs
@@ -1,8 +1,8 @@
 #![allow(non_snake_case, non_camel_case_types)]
 use super::utils::*;
 use std::arch::asm;
-use winapi::shared::minwindef::{BYTE, DWORD, FARPROC, HMODULE, ULONG, WORD};
-use winapi::shared::ntdef::{LPCSTR, LPCWSTR, PVOID, ULONGLONG, UNICODE_STRING};
+use winapi::shared::minwindef::{BYTE, DWORD, FARPROC, HINSTANCE__, HMODULE, ULONG, WORD};
+use winapi::shared::ntdef::{HANDLE, LPCSTR, LPCWSTR, NULL, PVOID, ULONGLONG, UNICODE_STRING};
 use winapi::um::winnt::IMAGE_DATA_DIRECTORY;
 
 pub fn string_to_lpcwstr(s: String) -> (Vec<u16>, *const u16) {
@@ -53,9 +53,9 @@ pub unsafe fn GetModuleHandle(lModuleName: LPCWSTR) -> Option<HMODULE> {
     None
 }
 
-pub unsafe fn GetProcAddress<F, P, R>(hModule: HMODULE, lpProcName: LPCSTR) -> Option<F>
+pub unsafe fn GetProcAddress<F>(hModule: HMODULE, lpProcName: LPCSTR) -> Option<F>
 where
-    F: Fn(P) -> R,
+    F: 'static,
 {
     crate::fake_exit!();
     let dosHeader = hModule as *const IMAGE_DOS_HEADER;
@@ -88,4 +88,73 @@ where
         }
     }
     None
+}
+
+pub unsafe fn GetProcAddressNative<F>(lpProcName: LPCSTR) -> Option<F>
+where
+    F: 'static,
+{
+    crate::fake_exit!();
+    let (_s, ps) = string_to_lpcwstr(String::from("C:\\Windows\\System32\\ntdll.dll"));
+    let hModuleOPT = GetModuleHandle(ps);
+    if hModuleOPT.is_none() {
+        return None;
+    }
+    let hModule = hModuleOPT.unwrap();
+
+    let dosHeader = hModule as *const IMAGE_DOS_HEADER;
+    let ntHeaders = (hModule as *const BYTE).wrapping_offset((*dosHeader).e_lfanew as isize)
+        as *const IMAGE_NT_HEADERS64;
+    let exportDirectory = (hModule as *const BYTE)
+        .wrapping_offset(((*ntHeaders).OptionalHeader.DataDirectory[0]).VirtualAddress as isize)
+        as *const IMAGE_EXPORT_DIRECTORY;
+    let addressOfFuntions: *const DWORD = (hModule as *const BYTE)
+        .wrapping_offset((*exportDirectory).AddressOfFunctions as isize)
+        as *const DWORD;
+    let addressOfNameOrdinals: *const WORD = (hModule as *const BYTE)
+        .wrapping_offset((*exportDirectory).AddressOfNameOrdinals as isize)
+        as *const WORD;
+    let addressOfNames: *const DWORD = (hModule as *const BYTE)
+        .wrapping_offset((*exportDirectory).AddressOfNames as isize)
+        as *const DWORD;
+    for i in 0..(*exportDirectory).NumberOfNames {
+        if strcmp_case_insens(
+            lpProcName,
+            (hModule as *const BYTE).wrapping_offset(*(addressOfNames.add(i as usize)) as isize)
+                as LPCSTR,
+        ) {
+            return Some(std::mem::transmute_copy(
+                &(hModule as *const BYTE).wrapping_offset(
+                    *addressOfFuntions.add(*addressOfNameOrdinals.add(i as usize) as usize)
+                        as isize,
+                ),
+            ));
+        }
+    }
+    None
+}
+
+type pRtlInitUnicodeString = fn(*mut UNICODE_STRING, LPCWSTR);
+type pLdrLoadDll = fn(LPCWSTR, ULONG, *mut UNICODE_STRING, *mut HMODULE);
+pub unsafe fn LoadLibrary(lpFileName: LPCWSTR) -> Option<HMODULE> {
+    let mut ustrModule: UNICODE_STRING;
+    let mut hModule: HMODULE = 0 as *mut HINSTANCE__;
+    let (_s, ps) = string_to_lpcstr(String::from("RtlInitUnicodeString"));
+    let (_s1, ps1) = string_to_lpcstr(String::from("LdrLoadDll"));
+    let RtlInitUnicodeString: pRtlInitUnicodeString = match GetProcAddressNative(ps) {
+        Some(proc) => proc,
+        None => return None,
+    };
+    RtlInitUnicodeString(&mut ustrModule as *mut UNICODE_STRING, lpFileName);
+    let LdrLoadDll: pLdrLoadDll = match GetProcAddressNative(ps1) {
+        Some(proc) => proc,
+        None => return None,
+    };
+    LdrLoadDll(
+        0 as *const u16,
+        0,
+        &mut ustrModule as *mut UNICODE_STRING,
+        &mut hModule as *mut HMODULE,
+    );
+    Some(hModule)
 }
